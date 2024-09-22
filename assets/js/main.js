1b05bf00c099f7de19c3a37236b80053b6609606
Fabien TAVERNIER <fabien.tavernier@oclock.io>
2024-09-22 18:43:17 +0200

    add title theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,8 @@ async function init() {
   const { theme, setup } = getURLParameters();
   const { CODE, STEPS } = await import(`./setups/setup-${theme}-${setup}.js`);
 
+  document.getElementById('title-theme').textContent = theme;
+
   animation.init(CODE, STEPS);
 }
 
